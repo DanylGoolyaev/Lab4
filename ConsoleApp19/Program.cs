f79ec7cd80c0a1fb15c590c77646a72af1fd69be
DanylGoolyaev <84317323+DanylGoolyaev@users.noreply.github.com>
2021-11-21 15:33:43 +0200

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp19
+{
+    public class FirstClass
+    {
+        public virtual void ShowInDebug(params object[] args)
+        {
+            Debug.Indent();
+            Debug.WriteLine("Arguments:");
+            Debug.WriteLine("");
+            Debug.Unindent();
+
+            foreach (var i in args)
+            {
+                Debug.WriteLine($"* {i}");
+            }
+            Debug.WriteLine("");
+        }
+    }
+    public class SecondClass : FirstClass
+    {
+        public override void ShowInDebug(params object[] args)
+        {
+            Debug.Indent();
+            Debug.WriteLine("Arguments in Second Class:");
+            Debug.WriteLine("");
+            Debug.Unindent();
+
+            foreach (var i in args)
+            {
+                Debug.WriteLine($"* {i.GetType()}: {i}");
+            }
+            Debug.WriteLine("");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstClass[] classes = new[]
+            {
+                new FirstClass(),
+                new SecondClass()
+            };
+
+            foreach (FirstClass test in classes)
+                test.ShowInDebug(
+                    DateTime.Now,
+                    884555612,
+                    1.547f,
+                    16.53d,
+                    11.42m,
+                    new object(),
+                    "Что-то"
+
+                );
+
+            Console.ReadLine();
+        }
+    }
+}
